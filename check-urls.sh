c11323eed918f5b1a6e3da48f86add690160c84f
Zoltán Herczog <zoltan.herczog@pm.me>
2022-06-30 00:50:10 +0200

    Fix status code filter and sophisticate status check

--- check-urls.sh
+++ check-urls.sh
@@ -7,12 +7,21 @@ fi
 URL="$1"
 
 echo "Checking $URL .."
-status_code=$(curl --max-time 10 --location 10 --silent --insecure --head "$URL" | grep HTTP/ | tail -1 |  awk {'print $2'});
-if [[ $status_code -ne 200 ]]; then
-    echo $URL >> invalid_urls.txt
-    echo -e "\x1B[31mError: $status_code \x1B[0m"
-    exit 2
+# status_code=$(curl --max-time 100 --location 10 --silent --insecure --head "$URL" | grep HTTP/ | tail -1 |  awk {'print $2'});
+status_code=$(curl --write-out '%{http_code}' --silent  --output /dev/null "$URL")
+
+if [[ $status_code -ge 200 && $status_code -le 299 ]];then
+    echo -e "\x1B[32mOK status code: $status_code for domain $URL  \x1B[0m"
+    exit 0
 fi
 
-echo -e "\x1B[32mOK \x1B[0m"
-exit 0
+if [[ $status_code -ge 300 && $status_code -le 399 ]];then
+    severity="Warning - Redirection - "
+else 
+    severity="Error"
+fi
+
+echo "Severity: $severity status code: $status_code for URL: $URL" >> invalid_urls.txt
+echo -e "\x1B[31mSeverity: $severity status code: $status_code for URL: $URL \x1B[0m"
+exit 2
+
